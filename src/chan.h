789b9fa2b59094b9009378ce270652083630a172
Jing Peng <pj.hades@gmail.com>
2021-04-21 18:03:28 -0400

    separate unbuf chan and buf chan

--- src/chan.h
+++ src/chan.h
@@ -4,43 +4,50 @@
 #include <stdbool.h>
 #include "mutex.h"
 
+struct unbuf_chan {
+    _Atomic bool closed;
+
+    // pointer used for data exchange.
+    _Atomic(void **) datap;
+
+    // guarantees that at most one writer and
+    // one reader have the right to access.
+    struct mutex send_mtx;
+    struct mutex recv_mtx;
+
+    // These futexes start from 1 (CHAN_NOT_READY).
+    // They are incremented to indicate that a thread is waiting.
+    // They are decremented to indicate that data exchange is done.
+    _Atomic uint32_t send_ftx;
+    _Atomic uint32_t recv_ftx;
+};
+
 struct chan_item {
     _Atomic uint32_t lap;
     void *data;
 };
 
-struct chan {
+struct buf_chan {
     _Atomic bool closed;
 
-    // Unbuffered channels only: the pointer used for data exchange.
-    _Atomic(void **) datap;
-
-    // Unbuffered channels only: guarantees that at most
-    // one writer and one reader have the right to access.
-    struct mutex send_mtx;
-    struct mutex recv_mtx;
-
-    // For unbuffered channels, these futexes start from 1 (CHAN_NOT_READY).
-    // They are incremented to indicate that a thread is waiting.
-    // They are decremented to indicate that data exchange is done.
-    //
-    // For buffered channels, these futexes represent credits for
-    // a reader or write to retry receiving or sending.
+    // These futexes represent credits for a reader or
+    // write to retry receiving or sending.
     _Atomic uint32_t send_ftx;
     _Atomic uint32_t recv_ftx;
 
-    // Buffered channels only: number of waiting threads on the futexes.
+    // Number of waiting threads on the futexes.
     _Atomic size_t send_waiters;
     _Atomic size_t recv_waiters;
 
     // Ring buffer
-    size_t cap;
     _Atomic uint64_t head;
     _Atomic uint64_t tail;
-    struct chan_item ring[0];
+    size_t cap;
+    struct chan_item *ring;
 };
 
 typedef void *(*chan_alloc_fn)(size_t);
+typedef void (*chan_free_fn)(void *);
 
 struct chan *chan_make(size_t cap, chan_alloc_fn alloc);
 struct chan chan_make_unbuf(void);
